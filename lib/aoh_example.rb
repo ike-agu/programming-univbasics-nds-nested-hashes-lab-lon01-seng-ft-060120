211e87ddb159b6b5a7517ccb55c74db148c0020b
ikenna agulobi <ikennaww@yahoo.fr>
2020-04-22 10:20:19 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -35,7 +35,7 @@ def aoh_update(aoh, row, key, new_value)
   # Update the AoH data at row and key to have the value of new_value
   # Return the updated AoH
   
-  aoh_lookup[row][key]= new_value
+  aoh[row][key]= new_value
   
-  aoh_lookup
+  aoh
 end
